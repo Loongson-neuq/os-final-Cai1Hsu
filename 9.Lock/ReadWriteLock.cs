58df2a845e1a814e00a19d40454932a8bd24ae94
Caiyi Shyu <cai1hsu@outlook.com>
2024-09-23 23:18:04 +0800

    Add try* API for Roslyn to utilize null checking

--- 9.Lock/ReadWriteLock.cs
+++ 9.Lock/ReadWriteLock.cs
@@ -1,6 +1,7 @@
 #pragma warning disable CS0420
 
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 // A simple read/write lock implementation
 class ReadWriteLock
@@ -8,6 +9,55 @@ class ReadWriteLock
     private volatile uint readRC = 0;
 
     private volatile uint writeRC = 0;
+
+    public bool TryAcquireReadLock([NotNullWhen(true)] out ReadPart? readPart)
+    {
+        readPart = null;
+
+        if (Volatile.Read(ref writeRC) is 0)
+        {
+            Interlocked.Increment(ref readRC);
+
+            // Someone acquired the write lock when we adding read lock reference counter
+            // We should give it up.
+            if (Volatile.Read(ref writeRC) is not 0)
+            {
+                Interlocked.Decrement(ref readRC);
+            }
+            else
+            {
+                readPart = new ReadPart(this);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool TryAcquireWriteLock([NotNullWhen(true)] out WritePart? writePart)
+    {
+        writePart = null;
+
+        if (Volatile.Read(ref readRC) is 0
+                && Volatile.Read(ref writeRC) is 0)
+        {
+            Interlocked.Increment(ref writeRC);
+
+            // Another callsite acquired the write lock or a read lock when we acquire write lock
+            // We should give it up.
+            if (Volatile.Read(ref readRC) != 0 || Volatile.Read(ref writeRC) > 1)
+            {
+                Interlocked.Decrement(ref writeRC);
+            }
+            else
+            {
+                writePart = new WritePart(this);
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public async Task<ReadPart?> AcquireReadLock(bool blockIfUnavaliable = false)
     {
